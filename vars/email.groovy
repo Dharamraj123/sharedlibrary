019461796af6a1aa3ed702094faeca962f615f43
Dharamraj123 <34584306+Dharamraj123@users.noreply.github.com>
2022-01-25 15:45:09 +0530

    Update email.groovy

--- vars/email.groovy
+++ vars/email.groovy
@@ -1,8 +1,10 @@
-def call() {
+#!/usr/bin/env groovy
+
+def call(String DEV_MAIL_ID = 'er.dj1990@gmail.com') {
    emailext (
         subject: "FAILED: Job at STAGE ${env.STAGE_NAME} ${env.JOB_NAME} - ${env.BUILD_NUMBER}",
         attachLog: true, attachmentsPattern: '*.log',
         body: """<br><p><p>Check console output at &QUOT;<a href='${env.BUILD_URL}console'>${env.JOB_NAME} - ${env.BUILD_NUMBER}</a>&QUOT;</p><br>""",
-        to: """${env.DEV_MAIL_ID}""", mimeType: 'text/html', recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+        to: """${DEV_MAIL_ID}""", mimeType: 'text/html', recipientProviders: [[$class: 'DevelopersRecipientProvider']]
     )
 }
